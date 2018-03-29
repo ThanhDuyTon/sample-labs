fd5c592fb3ecf2055f6d758fe7c4e62452a73826
Thanh Duy Ton <thaton@cisco.com>
2018-03-28 18:59:12 -0700

    Fixed JS

--- labs/os-support/assets/sample.js
+++ labs/os-support/assets/sample.js
@@ -2,8 +2,8 @@ setTimeout(function () {
   var platform = window.navigator.platform;
 
   // Native JS
-  document.querySelector(".sample-2").write(platform);
+  document.querySelector("#sample-2").innerHTML = platform;
 
   // jQuery
-  $('.sample-3').text(platform);
+  $('#sample-3').text(platform);
 }, 5000);
