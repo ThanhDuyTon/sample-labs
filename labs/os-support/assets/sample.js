8c1b6cac5d5929f523c7a89262ec6d9139753523
Thanh Duy Ton <thaton@cisco.com>
2018-03-28 18:25:48 -0700

    Need to wrap script timeout

--- labs/os-support/assets/sample.js
+++ labs/os-support/assets/sample.js
@@ -1,4 +1,9 @@
-var platform = window.navigator.platform;
+setTimeout(function () {
+  var platform = window.navigator.platform;
 
-// Native JS
-document.querySelector(".sample-2").write(platform);
+  // Native JS
+  document.querySelector(".sample-2").write(platform);
+
+  // jQuery
+  $('.sample-3').text(platform);
+}, 5000);
